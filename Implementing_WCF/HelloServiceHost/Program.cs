703d363596e9f8990c41eb76624e227fdbdc40b8
singhanant042 <anant.singh@hpe.com>
2025-04-28 15:37:12 +0530

    Implementing WCF- contains service,host,and client

---
+++ Implementing_WCF/HelloServiceHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+
+
+namespace HelloServiceHost
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                ServiceMetadataBehavior serviceMetadataBehavior = new ServiceMetadataBehavior()
+                {
+                    HttpGetEnabled = true,
+                };
+
+                host.Description.Behaviors.Add(serviceMetadataBehavior);
+                host.AddServiceEndpoint(typeof(HelloService.IHelloService), new NetTcpBinding(), "HelloService");
+                //host.AddServiceEndpoint(typeof(HelloService.IHelloService), new BasicHttpBinding , "HelloService");
+
+
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}

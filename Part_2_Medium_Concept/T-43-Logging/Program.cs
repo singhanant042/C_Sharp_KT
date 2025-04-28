827d4b8c455d37ef6f27d0367794c85f5963780b
singhanant042 <anant.singh@hpe.com>
2025-04-28 15:26:42 +0530

    .

---
+++ Part_2_Medium_Concept/T-43-Logging/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Security.Cryptography;
+
+namespace Log4net_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log=log4net.LogManager.GetLogger(typeof(Program));
+
+            try
+            {
+                Console.WriteLine("Enter the value");
+                int x = int.Parse(Console.ReadLine()):
+                int y = int.Parse(Console.ReadLine());
+
+                int result = x / y;
+
+                log.Info("Result={0}", result);
+            }
+            catch(Exception e)
+            {
+                log.Error(e.Message);
+            }
+
+
+        }
+    }
+}
